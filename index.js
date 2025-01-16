f412f9e86f58b19da3b3cb6be1ba2c7875a8e1f3
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-01-16 13:00:57 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(freeSample){
+  if(freeSample <= 400){
+      return 'This one is on me!'
+  } else if(freeSample > 400 && freeSample < 2000){
+      return 'That will be twenty bucks.'
+  } else if(freeSample > 2000 && freeSample < 2500){
+      return 'I will gladly take your thirty bucks.'
+  } else {
+      return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destinationCity){
+  return destinationCity === 'NYC'?'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips){
+  switch(tips){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
